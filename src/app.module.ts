bccd156dc4240f16d297c7b0f93bb9e8d46ac527
Brixter Porras <brixterporras@gmail.com>
2024-12-04 19:53:40 +0800

    fix: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import configuration from './config/configuration';
+import databaseConfig from './config/database.config';
 import { ListingModule } from './listing/listing.module';
 import { MarketplaceModule } from './marketplace/marketplace.module';
 import { SaleModule } from './sale/sale.module';
@@ -11,7 +13,7 @@ import { StoreModule } from './store/store.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // load: [configuration, databaseConfig],
+      load: [configuration, databaseConfig],
       // validationSchema: configSchema,
       // validationOptions: {
       //   allowUnknown: false,
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './AppModule';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  console.log(configService);
   const port = configService.get('PORT');
 
+  console.log(port);
   await app.listen(port);
 }
 bootstrap();

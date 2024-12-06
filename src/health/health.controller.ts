c18a699f31ca43b1bde39553f17bd9a7146604bf
Brixter Porras <brixterporras@gmail.com>
2024-12-06 15:12:38 +0800

    feat: added memory health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,6 +3,7 @@ import {
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
+  MemoryHealthIndicator,
   MikroOrmHealthIndicator,
 } from '@nestjs/terminus';
 
@@ -10,6 +11,7 @@ import {
 export class HealthController {
   constructor(
     private health: HealthCheckService,
+    private memory: MemoryHealthIndicator,
     private http: HttpHealthIndicator,
     private mikroOrm: MikroOrmHealthIndicator,
   ) {}
@@ -18,6 +20,8 @@ export class HealthController {
   @HealthCheck()
   check() {
     return this.health.check([
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () => this.memory.checkRSS('memory_rss', 300 * 1024 * 1024),
       () => this.mikroOrm.pingCheck('database'),
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
     ]);

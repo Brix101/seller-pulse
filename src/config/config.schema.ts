01f6b20543b5a29ad4b49d7cd025eec8216a16a7
Brixter Porras <brixterporras@gmail.com>
2024-12-06 15:06:37 +0800

    feat: added redis config

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -4,7 +4,7 @@ const configSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
   DATABASE_HOST: z.string(),
   DATABASE_PORT: z.coerce.number().default(5432),
   DATABASE_USER: z.string(),

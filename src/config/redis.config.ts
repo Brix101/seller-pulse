3048c7adec295970af59c0ec0bcc36cf1d036d2f
Brixter Porras <brixterporras@gmail.com>
2024-12-06 14:45:06 +0800

    refactor: redis ssl config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -4,5 +4,5 @@ export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT, 10) || 6379,
   password: process.env.REDIS_PASSWORD,
-  sslEnabled: process.env.REDIS_SSL_ENABLED === 'true',
+  ssl: process.env.REDIS_SSL === 'true',
 }));

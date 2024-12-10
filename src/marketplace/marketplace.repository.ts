a323d2e6b37583b1ab49e128681cc2c2794fa907
Brixter Porras <brixterporras@gmail.com>
2024-12-10 20:13:18 +0800

    refactor: to trpc

--- src/marketplace/marketplace.repository.ts
+++
@@ -1,4 +0,0 @@
-import { EntityRepository } from '@mikro-orm/postgresql';
-import { Marketplace } from './entities/marketplace.entity';
-
-export class MarketplaceRepository extends EntityRepository<Marketplace> {}
--- src/store/store.repository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository } from '@mikro-orm/postgresql';
-import { Store } from './entities/store.entity';
-
-export class StoreRepository extends EntityRepository<Store> {
-  // Add custom methods here
-}

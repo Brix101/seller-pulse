cd832a035a14e46d275a70d2ae4e67a381fd0f32
Brixter Porras <brixterporras@gmail.com>
2024-12-11 17:18:15 +0800

    feat: added eslint react config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -5,7 +5,7 @@ import reactConfig from "@sellerpulse/eslint-config/react";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [".next/**"],
+    ignores: [".dist/**"],
   },
   ...baseConfig,
   ...reactConfig,
